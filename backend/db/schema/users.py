25ca1d4ccec8cc04c8de836b35df2efa0fd67fc8
Abdessamad Touzani <83804777+AbdessamadTzn@users.noreply.github.com>
2025-03-23 00:25:39 +0100

    Table User

--- backend/db/schema/users.py
+++ backend/db/schema/users.py
@@ -6,7 +6,6 @@ def create_users_table(cursor):
         password TEXT NOT NULL,
         first_name TEXT,
         last_name TEXT,
-        num_ticket TEXT REFERENCES tickets(num_ticket),
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
     """)
